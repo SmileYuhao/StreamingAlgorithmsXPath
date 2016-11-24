ffd5f36aa7bf0dfd1ca406140a89b1fe05cb2140
SmileYuhao <prayer_zhao@outlook.com>
2016-11-24 19:53:35 +0100

    Add method in interface

--- src/StreamingAlgorithmXPath/StreamingAlgorithm.java
+++ src/StreamingAlgorithmXPath/StreamingAlgorithm.java
@@ -1,6 +1,8 @@
 package StreamingAlgorithmXPath;
 
 public interface StreamingAlgorithm {
+    void startDocument(String query);
+
     void processXmlLine(String line);
 
     String getNodeNumber();
